b1efd3f592cab4929e84a4427228e869730229a4
baiyo <119372694+baiyo@users.noreply.github.com>
2025-05-03 01:20:27 +1200

    commit one

--- src/src.lua
+++ src/src.lua
@@ -1,7 +1,35 @@
+local RunService = game:GetService("RunService")
 local Types = require(script.Parent.types)
 
 local EffectManager = {}
 
 EffectManager.__index = EffectManager
+
+function EffectManager.new()
+	local self = setmetatable({}, EffectManager)
+
+    self.Effects = {
+        [Instance]: {
+            [string]: {
+                value: Types.Acceptable | {start: number, duration: number},
+                extra: {[any]: any}
+            }
+        }
+    }
+
+    self.Listeners = {
+        Heartbeat = {} :: {[string]: {
+            Callback:(Instance, Types.Acceptable | nil) -> nil,
+        }} | {},
+
+        Stepped = {} :: {[string]: {
+            Callback:(number, Instance, Types.AcceptableState | nil) -> nil,
+        }} | {}
+    }
+
+	return self
+end
+
+export type Class = typeof(EffectManager.new())
 
 return EffectManager
